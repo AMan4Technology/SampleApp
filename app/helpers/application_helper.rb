702a9b98372c37a7c04de25be441eccac31256bf
AMan <35390271+aman4technology@users.noreply.github.com>
2018-03-27 23:02:15 +0800

    ruby on rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # 根据所在页面返回完整的标题
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    return base_title if page_title.empty?
+    page_title + " | " + base_title
+  end
 end
